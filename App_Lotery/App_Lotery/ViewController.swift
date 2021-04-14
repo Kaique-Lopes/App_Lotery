0cbc097e7f9ea7def04691384c03d18390956edf
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-04-13 23:10:48 -0300

    outlets

--- App_Lotery/App_Lotery/ViewController.swift
+++ App_Lotery/App_Lotery/ViewController.swift
@@ -8,11 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lbGameType: UILabel!
+    @IBOutlet weak var scGameType: UISegmentedControl!
+    @IBOutlet var balls: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    @IBAction func generateGame() {
+    }
+    
 
 }
 
